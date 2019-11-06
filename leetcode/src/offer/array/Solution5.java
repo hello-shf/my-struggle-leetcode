f4b98696d44b11c7ce1dba55075ec6705b7a78a4
sunhuaifeng <877635632@qq.com>
2019-11-06 22:08:30 +0800

    剑指offer

---
+++ leetcode/src/offer/array/Solution5.java
@@ -0,0 +1,32 @@
+package offer.array;
+
+/**
+ * 描述：替换空格
+ * 请实现一个函数，把字符串中的每个空格替换成"%20"。
+ *
+ * 你可以假定输入字符串的长度最大是1000。
+ * 注意输出字符串的长度可能大于1000。
+ *
+ * @Author shf
+ * @Date 2019/11/6 21:32
+ * @Version V1.0
+ **/
+public class Solution5 {
+    public static String replaceSpaces(StringBuffer str) {
+        StringBuffer sb = new StringBuffer();
+        char[] chars = str.toString().toCharArray();
+        for (Character cha : chars) {
+            if (cha == ' ') {
+                sb.append("%20");
+            }else{
+                sb.append(cha);
+            }
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("We are happy.");
+        String s = replaceSpaces(sb);
+        System.out.println(s);
+    }
+}
---
+++ leetcode/src/offer/array/Solution6.java
@@ -0,0 +1,48 @@
+package offer.array;
+
+import common.ListNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 描述：从尾到头打印链表
+ * 输入一个链表的头结点，按照 从尾到头 的顺序返回节点的值。
+ *
+ * 返回的结果用数组存储。
+ *
+ * 样例
+ * 输入：[2, 3, 5]
+ * 返回：[5, 3, 2]
+ *
+ * @Author shf
+ * @Date 2019/11/6 21:51
+ * @Version V1.0
+ **/
+public class Solution6 {
+    public static int[] printListReversingly(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+        printListReversingly(head, list);
+        int[] nums = new int[list.size()];
+        for(int i =0; i<list.size(); i++)
+            nums[i] = list.get(i);
+        return nums;
+    }
+    public static void printListReversingly(ListNode head, List<Integer> list) {
+        if(head == null){
+            return;
+        }
+        printListReversingly(head.next, list);
+        //递归语句之后，即递归深度 --，递归语句之前递归深度 ++ 的天然特性。即在递归语句之后，链表是从尾到头的顺序。
+        list.add(head.val);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4};
+        ListNode root = new ListNode(arr);
+        int[] ints = printListReversingly(root);
+        for (int i : ints)
+            System.out.print(i);
+        System.out.println();
+    }
+}
